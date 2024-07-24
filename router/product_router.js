7a451dfef214968fa0c44ec3d6dae8ae31ee38f2
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-07-24 21:53:19 +0500

    added some changes

--- router/product_router.js
+++ router/product_router.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const { getAllProducts, createProduct, deleteProduct, updateProduct, getOneProduct } = require("../controller/product_ctr");
+const requireAuth = require("../middleware/tokenChecker");
 
 const productRouter = Router();
 
 productRouter.get("/get_all_products", getAllProducts);
 productRouter.get("/get_one_product/:id", getOneProduct)
-productRouter.post("/add_product", createProduct)
-productRouter.delete("/delete_product/:id", deleteProduct)
-productRouter.put("/update_product/:id", updateProduct)
+productRouter.post("/add_product", requireAuth, createProduct)
+productRouter.delete("/delete_product/:id", requireAuth, deleteProduct)
+productRouter.put("/update_product/:id", requireAuth, updateProduct)
 
 module.exports = productRouter;
