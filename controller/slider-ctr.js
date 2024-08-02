caf7d43c3b38d6cae7ccbe2638b32d4480b9fcc2
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-08-02 18:15:21 +0500

    added get api for slide

--- controller/slider-ctr.js
+++ controller/slider-ctr.js
@@ -1,5 +1,14 @@
 const { Slide } = require("../model");
 
+const getSlides = async (_, res, next) => {
+  try {
+    const slides = await Slide.find();
+    return res.status(200).json(slides)
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createSlide = async (req, res, next) => {
   try {
     const { image } = req.body;
@@ -15,5 +24,6 @@ const createSlide = async (req, res, next) => {
 };
 
 module.exports = {
+  getSlides,
   createSlide,
 };
--- router/slider_router.js
+++ router/slider_router.js
@@ -1,9 +1,10 @@
 const Router = require("express")
 const requireAdmin = require("../middleware/tokenChecker")
-const { createSlide } = require("../controller/slider-ctr")
+const { createSlide, getSlides } = require("../controller/slider-ctr")
 
 const slideRouter = Router()
 
 slideRouter.post("/create_slide", requireAdmin, createSlide)
+slideRouter.get("/get_slides", getSlides)
 
 module.exports = slideRouter
